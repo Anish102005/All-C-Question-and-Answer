6f92f28eb8e531f4f5e53832ca923774024fd4fb
Anish102005 <anishuem102005@gmail.com>
2025-08-10 23:23:49 +0530

    Create Print matrix in spiral form.c

---
+++ Print matrix in spiral form.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define ROWS 4
+#define COLS 4
+
+void printSpiral(int matrix[ROWS][COLS]) {
+    int top = 0, bottom = ROWS - 1;
+    int left = 0, right = COLS - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            printf("%d ", matrix[top][i]);
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", matrix[i][right]);
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                printf("%d ", matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", matrix[i][left]);
+            }
+            left++;
+        }
+    }
+}
+
+int main() {
+    int matrix[ROWS][COLS] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    printSpiral(matrix);
+    return 0;
+}
